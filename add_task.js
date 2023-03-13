b51283a2e1288c2f2415363634e453f060219493
Patrick171717 <patrick1285@gmx.de>
2023-03-13 10:23:50 +0100

    add_task

--- add_task.js
+++ add_task.js
@@ -1,13 +1,24 @@
 async function addTaskToUser() {
+  let taskBoardList = document.getElementById('taskBoardList');
   let taskTitle = document.getElementById('taskTitle');
-  // let taskTitle = document.getElementById('taskTitle');
-  // let taskTitle = document.getElementById('taskTitle');
-  // let taskTitle = document.getElementById('taskTitle');
-  // let taskTitle = document.getElementById('taskTitle');
+  let taskDescription = document.getElementById('taskDescription');
+  let taskCategory = document.getElementById('taskCategory');
+  let taskAssigned = document.getElementById('taskAssigned');
+  let taskDate = document.getElementById('taskDate');
+  let taskButtonUrgent = document.getElementById('taskButtonUrgent');
+  let ttaskButtonMedium = document.getElementById('taskButtonMedium');
+  let taskButtonLow = document.getElementById('taskButtonLow');
   let subtaskTitle = document.getElementById('subtaskTitle');
 
   let newTask = {
+    boardList: taskBoardList.value,
     title: taskTitle.value,
+    description: taskDescription.value,
+    category: taskCategory.value,
+    assigned_to: taskAssigned.value,
+    date: taskDate.value,
+
+
     subtasks: [
       {
         subtaskTitle: subtaskTitle.value,
@@ -20,3 +31,14 @@ async function addTaskToUser() {
   loadUsers();
 }
 
+function taskClearButton () {
+   document.getElementById('taskBoardList').value = '';
+   document.getElementById('taskTitle').value = '';
+   document.getElementById('taskDescription');
+   document.getElementById('taskCategory');
+   document.getElementById('taskAssigned');
+   document.getElementById('taskDate');
+   document.getElementById('taskButtonUrgent');
+   document.getElementById('taskButtonMedium');
+   document.getElementById('taskButtonLow');
+}
