b6b5cfea78e404dc9d611c8024218ef33d78de2a
Patrick171717 <patrick1285@gmx.de>
2023-03-15 07:46:52 +0100

    add_task änderungen

--- add_task.js
+++ add_task.js
@@ -1,3 +1,27 @@
+async function initTasks() {
+  setURL('https://gruppe-05i.developerakademie.net/smallest_backend_ever');
+  await downloadFromServer();
+  userData = await JSON.parse(backend.getItem('users')) || [];
+  getCurrentUser();
+  let currentUserData = userData[currentUser];
+  showCurrentUser(currentUser, currentUserData);
+  // TODO Alle Boards abfragen!!
+  if (
+    userData[currentUser].board[0].boardlistTasks.length > 0 ||
+    userData[currentUser].board[1].boardlistTasks.length > 0 ||
+    userData[currentUser].board[2].boardlistTasks.length > 0 ||
+    userData[currentUser].board[3].boardlistTasks.length > 0
+  ) {
+    renderTaskForm();
+    generateContactDropdown();
+  } else {
+    console.warn('No tasks found');
+    // templatefirstContact();
+  }
+}
+
+
+
 const taskBoardList = document.getElementById('taskBoardList');
 const taskTitle = document.getElementById('taskTitle');
 const taskDescription = document.getElementById('taskDescription');
@@ -8,12 +32,49 @@ const taskButtonUrgent = document.getElementById('taskButtonUrgent');
 const taskButtonMedium = document.getElementById('taskButtonMedium');
 const taskButtonLow = document.getElementById('taskButtonLow');
 
+
+function renderTaskForm() {
+  taskBoardList.innerHTML = ``;
+  taskBoardList.innerHTML += `
+  <option disabled selected hidden>Select board list</option>
+        <option value="0">${userData[currentUser].board[0].boardlistTitle}</option>
+        <option value="1">${userData[currentUser].board[1].boardlistTitle}</option>
+        <option value="2">${userData[currentUser].board[2].boardlistTitle}</option>
+        <option value="3">${userData[currentUser].board[3].boardlistTitle}</option>
+  `;
+  console.log("Jane is da!!");
+}
+
+
+function generateContactDropdown() {
+  taskAssigned.innerHTML = ``;
+  taskAssigned.innerHTML += `
+  <option disabled selected hidden>Select Contacts to assign</option>
+  <option id="contactsOptions"></option>
+  <option value="${userData[currentUser].contacts[0].firstName} 
+  ${userData[currentUser].contacts[0].lastName}">
+  ${userData[currentUser].contacts[0].firstName} 
+  ${userData[currentUser].contacts[0].lastName}</option> `;
+
+  for (let i = 0; i < userData[currentUser].contacts.length; i++) {
+    const contactsOptions = document.getElementById('contactsOptions');
+    // contactsOptions.innerHTML = ``;
+    contactsOptions.innerHTML += `
+    <option disabled selected hidden value="${userData[currentUser].contacts[i].firstName} 
+  ${userData[currentUser].contacts[i].lastName}">
+  ${userData[currentUser].contacts[i].firstName} 
+  ${userData[currentUser].contacts[i].lastName}</option>
+    `;
+  }
+}
+
+
 // TODO  Übergabe des Button Text
-//Zahl übergeben 
+
 async function addTaskToUser() {
 
   let newTask = {
-    boardList: taskBoardList.value,
+    boardList: taskBoardList.value, //Zahl übergeben DONE!
     title: taskTitle.value,
     description: taskDescription.value,
     category: taskCategory.value,
@@ -57,16 +118,8 @@ function setPriority() {
 
 
 
-// function generateContactDropdown(userData) {
-//   const dropdownContacts = document.getElementById("taskAssigned");
-//   dropdownContacts.innerHTML = '';
 
-//   for (let i = 0; i < userData[0].contacts[0].avatar_initials.length; i++) {
-//     const contactList =
-//   }
-// }
 
-// userData[0].contacts[0].avatar_initials
 
 
 
